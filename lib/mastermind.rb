53c5999b049d07e8e497ea14ababd3a639798b68
Brian Zanti <brianzanti@gmail.com>
2018-05-08 18:04:37 -0600

    WIP through check_positions

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -7,15 +7,43 @@ class Mastermind
               :end_time
 
   def initialize(difficulty = :beginner)
+    @difficulty = difficulty
+    @start_time = Time.now
+    @num_guesses = 0
+    @answer = generate_answer
   end
 
-  def generate_answer(difficulty)
+  def generate_answer
+    colors = possible_colors.chars
+    length = answer_length
+    answer = ""
+    length.times do
+      answer << colors.sample
+    end
+    answer
+  end
+
+  def possible_colors
+    return 'rgby' if @difficulty == :beginner
+    return 'rgbyv' if @difficulty == :intermediate
+    return 'rgbyvo' if @difficulty == :advanced
+  end
+
+  def answer_length
+    return 4 if @difficulty == :beginner
+    return 6 if @difficulty == :intermediate
+    return 8 if @difficulty == :advanced
   end
 
   def guess(guess, answer)
   end
 
-  def check_positions(guess, answer)
+  def check_positions(guess, answer = @answer)
+    positions = 0
+    guess.length.times do |index|
+      positions += 1 if guess[index] == answer[index]
+    end
+    positions
   end
 
   def check_elements(guess, answer)
